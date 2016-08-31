82f726ae34f4bc82be5b39211527c5462563dff5
Saulo Carvalho <sfcarvalho@gmail.com>
2016-08-31 14:37:26 -0300

    Adding CSS and JS files loading properly

--- app/src/ChamadosBundle/Resources/public/js/main.js
+++ app/src/ChamadosBundle/Resources/public/js/main.js
@@ -1,37 +1,34 @@
-/**
- * Created by dev on 31/08/16.
- */
 
 
 function initAjaxForm()
 {
-    alert('ajax');
-    // $('body').on('submit', '.ajaxForm', function (e) {
-    //
-    //     e.preventDefault();
-    //
-    //     $.ajax({
-    //         type: $(this).attr('method'),
-    //         url: $(this).attr('action'),
-    //         data: $(this).serialize()
-    //     })
-    //         .done(function (data) {
-    //             if (typeof data.message !== 'undefined') {
-    //                 alert(data.message);
-    //             }
-    //         })
-    //         .fail(function (jqXHR, textStatus, errorThrown) {
-    //             if (typeof jqXHR.responseJSON !== 'undefined') {
-    //                 if (jqXHR.responseJSON.hasOwnProperty('form')) {
-    //                     $('#form_body').html(jqXHR.responseJSON.form);
-    //                 }
-    //
-    //                 $('.form_error').html(jqXHR.responseJSON.message);
-    //
-    //             } else {
-    //                 alert(errorThrown);
-    //             }
-    //
-    //         });
-    // });
+
+    $('body').on('submit', '.ajaxForm', function (e) {
+
+        e.preventDefault();
+
+        $.ajax({
+            type: $(this).attr('method'),
+            url: $(this).attr('action'),
+            data: $(this).serialize()
+        })
+            .done(function (data) {
+                if (typeof data.message !== 'undefined') {
+                    alert(data.message);
+                }
+            })
+            .fail(function (jqXHR, textStatus, errorThrown) {
+                if (typeof jqXHR.responseJSON !== 'undefined') {
+                    if (jqXHR.responseJSON.hasOwnProperty('form')) {
+                        $('#form_body').html(jqXHR.responseJSON.form);
+                    }
+
+                    $('.form_error').html(jqXHR.responseJSON.message);
+
+                } else {
+                    alert(errorThrown);
+                }
+
+            });
+    });
 }
